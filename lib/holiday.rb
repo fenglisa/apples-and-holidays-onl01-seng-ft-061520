4240cf254da2300ae5be90dd7c85f77dbe1d2612
Lisa Feng <lisafeng10@gmail.com>
2020-06-23 01:10:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,6 +77,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday.each do |holiday, supplies|
       if supplies.include?("BBQ")
         holidaysWithBBQ  << holiday
+      end
     end
   end
   holidaysWithBBQ
