477279291cb50b132b1d6c9fc796ef93f139f258
Lisa Feng <lisafeng10@gmail.com>
2020-06-23 00:57:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,9 +61,9 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash[season].each do |holiday, supplies|
       holidayName = holiday.to_s
       if holidayName.include?("_")
-        puts "  #{holiday.capitalize.split("_").join(" ")}: #{supplies.join(", ")}"
+        puts "  #{holidayName.capitalize.split("_").join(" ")}: #{supplies.join(", ")}"
       else
-         puts "  #{holiday.capitalize}: #{supplies.join(", ")}"
+         puts "  #{holidayName.capitalize}: #{supplies.join(", ")}"
       end
     end
   end
