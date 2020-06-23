b7c8543ffb812cceb9de431228bf4218284693b8
Lisa Feng <lisafeng10@gmail.com>
2020-06-23 01:10:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,11 +72,14 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holidaysWithBBQ = []
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supplies|
-      
+      if supplies.include?("BBQ")
+        holidaysWithBBQ  << holiday
     end
   end
+  holidaysWithBBQ
 end
 
 
