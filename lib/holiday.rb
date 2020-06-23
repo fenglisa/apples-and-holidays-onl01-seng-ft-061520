a6bedf3da26325c943dbcfede800dce2354bdf24
Lisa Feng <lisafeng10@gmail.com>
2020-06-23 00:42:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday_hash[season].each do |holiday, supplies|
-      puts "  #{holiday.capitalize.tr!('_', ' ')}: #{supplies.join(", ")}"
+      puts "  #{holiday.capitalize.gsub("_", " ")}: #{supplies.join(", ")}"
     end
   end
 end
